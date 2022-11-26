56be9f6ebd54a8cf3894c5d9fd685983d23a0f61
10956155 <dhruv.sgit@gmail.com>
2022-11-26 20:06:02 +0530

    challenge 1 Thapa channel...

--- practicereact/src/index.js
+++ practicereact/src/index.js
@@ -1,9 +1,40 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+//var React = require('react');
+//var ReactDOM = require('react-dom');
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(
+    <>
+        <h1> Netflix Series </h1>
+        <p> List of 5 series... </p>
+        <ol>
+            <li>Dark</li>
+            <li>DDLJ</li>
+            <li>Dahan</li>
+            <li>Masti</li>
+            <li>Hey Shona</li>
+        </ol>
+    </> , document.getElementById("root"))
+
+//import React from "react";
+// import ReactDOM from "react-dom"
 
 //ReactDOM.render('Kya dikhana hai (What to show..)', 'kaha dikhana hai (When to show..)', 'callback function');
 
-ReactDOM.render(<h1>Hello World!!!!!!!</h1>, document.getElementById('root'));
+//ReactDOM.render(
+  //  <React.Fragment><h1>Hello World!!!</h1> <p> Hello world paragraph is here....</p> </React.Fragment> , document.getElementById("root"));
+
+//ReactDOM.render(<><p>Netflix pack</p> <h1>List of 5 best series...</h1> </>, document.getElementById("root"));
+
+// ReactDOM.render(
+//     <div>
+//         <h1>Hello World!!!</h1>
+//         <p> Hello world paragraph is here....</p>
+//     </div>  , document.getElementById("root"));
+
+
+//ReactDOM.render(<h1>Hello World!!</h1>, document.getElementById('root'));
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
 // import './index.css';
