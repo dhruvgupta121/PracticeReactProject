77f388cac6405b6b190829d238acb408e40915d6
10956155 <dhruv.sgit@gmail.com>
2022-11-26 18:34:10 +0530

    Hello World changes.

--- practicereact/src/index.js
+++ practicereact/src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+var React = require('react');
+var ReactDOM = require('react-dom');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//ReactDOM.render('Kya dikhana hai (What to show..)', 'kaha dikhana hai (When to show..)', 'callback function');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<h1>Hello World!!!!!!!</h1>, document.getElementById('root'));
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+//
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     <h1> Hello World!!!</h1>
+//   // <React.StrictMode>
+//   //   <App />
+//   // </React.StrictMode>
+// );
+//
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
